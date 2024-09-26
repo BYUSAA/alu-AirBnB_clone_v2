923e2c6dfeb4af5a4f4bf535276500df58d7f28c
BYUUSAA <byuussaa@gmail.com>
2024-09-26 10:24:42 +0100

    updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-#web_static development
+# This is a bash script for setting up a basic web static environment for development.
 
+# Updating and upgrading the system to ensure all packages are up to date.
 sudo apt-get -y update
 sudo apt-get -y upgrade
+
+# Installing Nginx, the web server, to serve our static content.
 sudo apt-get -y install nginx
+
+# Creating directories for the web static files: 'releases/test' will hold the versioned files,
+# and 'shared' is for files that may be used across releases.
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Creating a simple test HTML file in the 'test' directory.
 echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
-# Adding the new configuration for /hbnb_static/0-index.html
+
+# Adding a new configuration: creating an HTML file for AirBnB clone with header and footer
+# that will be displayed at /hbnb_static/0-index.html.
 echo "<!DOCTYPE html>
 <html lang=\"en\">
     <head>
@@ -22,10 +32,18 @@ echo "<!DOCTYPE html>
         </footer>
     </body>
 </html>" | sudo tee /data/web_static/releases/test/0-index.html
-# End of new configuration
+# End of the new configuration file.
 
+# Removing any existing symbolic link to 'current' and recreating it to point to the 'test' release.
 sudo rm -rf /data/web_static/current
 sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Changing ownership of the '/data' directory to the user 'ubuntu', so they have full access.
 sudo chown -R ubuntu:ubuntu /data/
+
+# Editing the Nginx default configuration file to add a new location block for '/hbnb_static'.
+# This makes the '/hbnb_static' URL serve content from '/data/web_static/current'.
 sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+# Restarting the Nginx service to apply the new configuration.
 sudo service nginx restart
