fb99c5d3db56943be3c9e07548554a22edeb1266
BYUUSAA <byuussaa@gmail.com>
2024-10-02 11:57:03 +0100

    Update console.py

--- console.py
+++ console.py
@@ -47,7 +47,7 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not line:
                 raise SyntaxError("** class name missing **")
-                
+
             my_list = line.split(" ")
 
             kwargs = {}
@@ -86,7 +86,7 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not line:
                 raise SyntaxError("** class name missing **")
-                
+
             my_list = line.split(" ")
             if my_list[0] not in self.__classes:
                 raise NameError()
@@ -118,7 +118,7 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not line:
                 raise SyntaxError("** class name missing **")
-                
+
             my_list = line.split(" ")
             if my_list[0] not in self.__classes:
                 raise NameError()
@@ -173,7 +173,7 @@ class HBNBCommand(cmd.Cmd):
         try:
             if not line:
                 raise SyntaxError("** class name missing **")
-                
+
             my_list = split(line, " ")
             if my_list[0] not in self.__classes:
                 raise NameError()
@@ -202,74 +202,4 @@ class HBNBCommand(cmd.Cmd):
         except KeyError:
             print("** no instance found **")
         except AttributeError:
-            print("** attribute name missing **")
-        except ValueError:
-            print("** value missing **")
-
-    def count(self, line):
-        """Count the number of instances of a class."""
-        counter = 0
-        try:
-            my_list = split(line, " ")
-            if my_list[0] not in self.__classes:
-                raise NameError()
-            objects = storage.all()
-            for key in objects:
-                name = key.split('.')
-                if name[0] == my_list[0]:
-                    counter += 1
-            print(counter)
-        except NameError:
-            print("** class doesn't exist **")
-
-    def strip_clean(self, args):
-        """Strips the argument and returns a string of command.
-        Args:
-            args: input list of args
-        Return:
-            returns string of arguments
-        """
-        new_list = [args[0]]
-        try:
-            my_dict = eval(
-                args[1][args[1].find('{'):args[1].find('}') + 1])
-        except Exception:
-            my_dict = None
-        if isinstance(my_dict, dict):
-            new_str = args[1][args[1].find('(') + 1:args[1].find(')')]
-            new_list.append(((new_str.split(", "))[0]).strip('"'))
-            new_list.append(my_dict)
-            return new_list
-        new_str = args[1][args[1].find('(') + 1:args[1].find(')')]
-        new_list.append(" ".join(new_str.split(", ")))
-        return " ".join(new_list)
-
-    def default(self, line):
-        """Retrieve all instances of a class and
-        retrieve the number of instances.
-        """
-        my_list = line.split('.')
-        if len(my_list) >= 2:
-            if my_list[1] == "all()":
-                self.do_all(my_list[0])
-            elif my_list[1] == "count()":
-                self.count(my_list[0])
-            elif my_list[1][:4] == "show":
-                self.do_show(self.strip_clean(my_list))
-            elif my_list[1][:7] == "destroy":
-                self.do_destroy(self.strip_clean(my_list))
-            elif my_list[1][:6] == "update":
-                args = self.strip_clean(my_list)
-                if isinstance(args, list):
-                    obj = storage.all()
-                    key = args[0] + ' ' + args[1]
-                    for k, v in args[2].items():
-                        self.do_update(key + ' "{}" "{}"'.format(k, v))
-                else:
-                    self.do_update(args)
-        else:
-            cmd.Cmd.default(self, line)
-
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+            print("** attribute name missing **
