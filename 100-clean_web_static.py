e734b334f44d3e035e69d2e41054d727afe23c5f
BYUSAA <byuussaa@gmail.com>
2024-10-02 09:56:56 +0100

    new adds again

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# script to delete out-of-date archives.
+""" Fabscript that deletes
+    out-of-date archives.
+"""
 import os
 from fabric.api import *
 
-env.hosts = ["54.82.5.102", "3.94.103.18"]
+env.hosts = ["54.174.230.101", "100.26.57.164"]
 
 
 def do_clean(number=0):
