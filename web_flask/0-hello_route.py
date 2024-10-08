fb9c1caf5e9e6eb04d3f3b5488c25b50470daaee
BYUUSAA <byuussaa@gmail.com>
2024-10-08 16:09:36 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Starts a simple Flask web application """
-from flask import Flask
+"""
+start Flask application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Display a greeting message on the root route """
-    return "Hello HBNB!"
 
-if __name__ == "__main__":
-    # Set the application to run on host 0.0.0.0 and port 5000
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
+    
