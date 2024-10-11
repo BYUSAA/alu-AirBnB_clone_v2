a08977fe861405cbe98252e2fd3a1975f3e26467
BYUSAA <byuussaa@gmail.com>
2024-10-11 13:44:27 +0100

    new codes change

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,86 +1,31 @@
 #!/usr/bin/python3
-"""Deploy static content"""
-from fabric.api import *
-import os
-import re
-from datetime import datetime
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-env.user = 'ubuntu'
-env.hosts = ['3.80.74.138', '3.88.68.105']
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.91.97.107', '3.95.175.50']
 
-def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder"""
-    local("mkdir -p versions")
-    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
-    if result.failed:
-        return None
-    return result
-
-def check_server_status():
-    """Check if the deployed page is accessible"""
-    for host in env.hosts:
-        result = run(f"curl -s -o /dev/null -w '%{{http_code}}' http://{host}/hbnb_static/0-index.html")
-        if result != "200":
-            print(f"Error: Expected HTTP 200 but got {result} on host {host}")
-            return False
-    return True
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not os.path.isfile(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
-    filename_regex = re.compile(r'[^/]+(?=\.tgz$)')
-    match = filename_regex.search(archive_path)
-
-    # Upload the archive to the /tmp/ directory of the web server
-    archive_filename = match.group(0)
-    result = put(archive_path, "/tmp/{}.tgz".format(archive_filename))
-    if result.failed:
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
         return False
-
-    # Uncompress the archive to the folder
-    # /data/web_static/releases/<archive filename without extension> on
-    # the web server
-    result = run("mkdir -p /data/web_static/releases/{}/".format(archive_filename))
-    if result.failed:
-        return False
-    result = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-                 .format(archive_filename, archive_filename))
-    if result.failed:
-        return False
-
-    # Delete the archive from the web server
-    result = run("rm /tmp/{}.tgz".format(archive_filename))
-    if result.failed:
-        return False
-
-    # Move content from web_static to release directory
-    result = run("mv /data/web_static/releases/{}/web_static/* "
-                 "/data/web_static/releases/{}/".format(archive_filename, archive_filename))
-    if result.failed:
-        return False
-    result = run("rm -rf /data/web_static/releases/{}/web_static"
-                 .format(archive_filename))
-    if result.failed:
-        return False
-
-    # Delete the symbolic link /data/web_static/current from the web server
-    result = run("rm -rf /data/web_static/current")
-    if result.failed:
-        return False
-
-    # Create a new symbolic link pointing to the new release
-    result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-                 .format(archive_filename))
-    if result.failed:
-        return False
-
-    # Verify that the deployment was successful by checking HTTP status
-    if not check_server_status():
-        return False
-
-    return True
-
+    
